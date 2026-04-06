20d50166b02466a9e883ae8cada0b7c579a75fe9
chris k <yovirinc@gmail.com>
2026-04-06 22:22:41 +0200

    refactor(layout): move pages to (main) route group

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,7 @@
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-grey-50 min-h-screen">{children}</div>;
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className='bg-grey-50 min-h-screen'>{children}</div>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import BottomNav from "./components/BottomNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,9 +20,7 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${geistSans.variable} h-full`}>
       <body className="flex flex-col mx-auto pb-16 max-w-md min-h-full">
-        <Header />
         {children}
-        <BottomNav />
       </body>
     </html>
   );
